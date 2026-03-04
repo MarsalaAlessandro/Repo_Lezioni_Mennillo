7f3aa8b220ed57e488be7c4f4cf0fdac1141b901
MarsalaAlessandro <alessandro.marsala@edu.immaginazioneelavoro.it>
2026-03-04 13:49:08 +0100

    1

---
+++ PythonLezioni/Lezione2_tipi_di_dato/01_Variabili.py
@@ -0,0 +1,33 @@
+# Questo è un commento
+# Cos'è una variabile?
+# è un contenitore che memorizza un valore. Questo valore può cambiare durante l'esecuzione del codice
+
+nome = "Alessandro"
+eta = 17
+corso = "tecnico informatico"
+altezza = 1.78
+
+#indipendentemente dal tipo che scelgo non viene dichiarato da nessuna parte:
+#questo comportamento è tipico dei linguaggi debolmente tipizzati
+
+# Esercizio: Istanzia 2 variabili (nome a tua scelta) di tipo intero. con queste 2 variabili elabora le 4 operazioni matematica, stampa in console con un print i risultati.
+
+x = 4
+y = 6
+
+print(x + y)
+print(x - y)
+print(x*y)
+print(x/y)
+
+#oppure
+
+somma = x + y
+sottrazione = x - y
+moltiplicazione = x * y
+Divisione = x / y
+
+print(somma)
+print(sottrazione)
+print(moltiplicazione)
+print(Divisione)
